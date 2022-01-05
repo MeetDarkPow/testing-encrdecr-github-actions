2a5b320e2383817b34d270f0fa4e9a2c3ec15ad3
Meet Bhatnagar <meet1708@gmail.com>
2022-01-06 00:24:40 +0530

    Update testmain.R

--- testmain.R
+++ testmain.R
@@ -1,12 +1,14 @@
-df <- data.frame(A=c(1,2,3), B=c("A", "B", "C"), C=c(T,F,F)) 
-df <- serialize(df, con=NULL, ascii=T) 
-enc <- gpg_encrypt(df, receiver="64B1D93B44627B8B") 
-writeBin(enc, "test.df.gpg")
+# df <- data.frame(A=c(1,2,3), B=c("A", "B", "C"), C=c(T,F,F)) 
+# df <- serialize(df, con=NULL, ascii=T) 
+# 
+# gpg_keygen(name = "meet", email = "meet1708@gmail.com", passphrase = NULL)
+# 
+# enc <- gpg_encrypt(df, receiver="F39E31FC4F15E5D7") 
+# writeBin(enc, "Data/test_df.gpg")
 
 
 
 
-gpg_keygen(name = "meet", email = "meet1708@gmail.com", passphrase = NULL)
 
 
 dec <- gpg_decrypt("test.df.gpg", as_text = FALSE)
